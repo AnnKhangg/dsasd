b04f88632bfe9d46a885f79c78e753fe95b650b4
khang bel <annkhangg08@gmail.com>
2025-12-06 08:28:52 +0700

    update

--- book.js
+++ book.js
@@ -9,11 +9,14 @@ const btnBack1 = document.getElementById("btnBack1");
 const btnNext1 = document.getElementById("btnNext1");
 
 btnBack1.addEventListener("click", () => {
+    bannerBook3.style.visibility = "visible";
+    bannerBook3.style.opacity = "1";
+
     bannerBook1.style.visibility = "hidden";
     bannerBook1.style.opacity = "0";
 
-    bannerBook2.style.visibility = "visible";
-    bannerBook2.style.opacity = "1";
+    bannerBook2.style.visibility = "hidden";
+    bannerBook2.style.opacity = "0";
 });
 
 btnNext1.addEventListener("click", () => {
@@ -22,6 +25,9 @@ btnNext1.addEventListener("click", () => {
 
     bannerBook2.style.visibility = "visible";
     bannerBook2.style.opacity = "1";
+
+    bannerBook3.style.visibility = "hidden";
+    bannerBook3.style.opacity = "0";
 });
 
 
@@ -33,6 +39,9 @@ btnBack2.addEventListener("click", () => {
     bannerBook2.style.visibility = "hidden";
     bannerBook2.style.opacity = "0";
 
+    bannerBook3.style.visibility = "hidden";
+    bannerBook3.style.opacity = "0";
+
     bannerBook1.style.visibility = "visible";
     bannerBook1.style.opacity = "1";
 });
@@ -41,6 +50,35 @@ btnNext2.addEventListener("click", () => {
     bannerBook2.style.visibility = "hidden";
     bannerBook2.style.opacity = "0";
 
+    bannerBook3.style.visibility = "visible";
+    bannerBook3.style.opacity = "1";
+
+    bannerBook1.style.visibility = "hidden";
+    bannerBook1.style.opacity = "0";
+});
+
+// Banner chuyển từ 3 đến 1 và 2
+const btnBack3 = document.getElementById("btnBack3");
+const btnNext3 = document.getElementById("btnNext3");
+
+btnBack3.addEventListener("click", () => {
+    bannerBook2.style.visibility = "visible";
+    bannerBook2.style.opacity = "1";
+
+    bannerBook3.style.visibility = "hidden";
+    bannerBook3.style.opacity = "0";
+
+    bannerBook1.style.visibility = "hidden";
+    bannerBook1.style.opacity = "0";
+})
+
+btnNext3.addEventListener("click", () => {
     bannerBook1.style.visibility = "visible";
     bannerBook1.style.opacity = "1";
-});
+
+    bannerBook3.style.visibility = "hidden";
+    bannerBook3.style.opacity = "0";
+
+    bannerBook2.style.visibility = "hidden";
+    bannerBook2.style.opacity = "0";
+})
