04469079aebbc1887dc65a30507a775bea0e7e8e
khang bel <annkhangg08@gmail.com>
2025-12-21 16:19:44 +0700

    12/21/2025

--- book.js
+++ book.js
@@ -9,52 +9,51 @@ const btnBack1 = document.getElementById("btnBack1");
 const btnNext1 = document.getElementById("btnNext1");
 
 btnBack1.addEventListener("click", () => {
-    bannerBook3.style.visibility = "visible";
-    bannerBook3.style.opacity = "1";
+  bannerBook3.style.visibility = "visible";
+  bannerBook3.style.opacity = "1";
 
-    bannerBook1.style.visibility = "hidden";
-    bannerBook1.style.opacity = "0";
+  bannerBook1.style.visibility = "hidden";
+  bannerBook1.style.opacity = "0";
 
-    bannerBook2.style.visibility = "hidden";
-    bannerBook2.style.opacity = "0";
+  bannerBook2.style.visibility = "hidden";
+  bannerBook2.style.opacity = "0";
 });
 
 btnNext1.addEventListener("click", () => {
-    bannerBook1.style.visibility = "hidden";
-    bannerBook1.style.opacity = "0";
+  bannerBook1.style.visibility = "hidden";
+  bannerBook1.style.opacity = "0";
 
-    bannerBook2.style.visibility = "visible";
-    bannerBook2.style.opacity = "1";
+  bannerBook2.style.visibility = "visible";
+  bannerBook2.style.opacity = "1";
 
-    bannerBook3.style.visibility = "hidden";
-    bannerBook3.style.opacity = "0";
+  bannerBook3.style.visibility = "hidden";
+  bannerBook3.style.opacity = "0";
 });
-
 
 // Banner Thứ 2 chuyển lại Banner thứ 1
 const btnBack2 = document.getElementById("btnBack2");
 const btnNext2 = document.getElementById("btnNext2");
 
 btnBack2.addEventListener("click", () => {
-    bannerBook2.style.visibility = "hidden";
-    bannerBook2.style.opacity = "0";
+  bannerBook2.style.visibility = "hidden";
+  bannerBook2.style.opacity = "0";
 
-    bannerBook3.style.visibility = "hidden";
-    bannerBook3.style.opacity = "0";
+  bannerBook3.style.visibility = "hidden";
+  bannerBook3.style.opacity = "0";
 
-    bannerBook1.style.visibility = "visible";
-    bannerBook1.style.opacity = "1";
+  bannerBook1.style.visibility = "visible";
+  bannerBook1.style.opacity = "1";
 });
 
 btnNext2.addEventListener("click", () => {
-    bannerBook2.style.visibility = "hidden";
-    bannerBook2.style.opacity = "0";
+  bannerBook2.style.visibility = "hidden";
+  bannerBook2.style.opacity = "0";
 
-    bannerBook3.style.visibility = "visible";
-    bannerBook3.style.opacity = "1";
+  bannerBook3.style.visibility = "visible";
+  bannerBook3.style.opacity = "1";
 
-    bannerBook1.style.visibility = "hidden";
-    bannerBook1.style.opacity = "0";
+  bannerBook1.style.visibility = "hidden";
+  bannerBook1.style.opacity = "0";
 });
 
 // Banner chuyển từ 3 đến 1 và 2
@@ -62,50 +61,49 @@ const btnBack3 = document.getElementById("btnBack3");
 const btnNext3 = document.getElementById("btnNext3");
 
 btnBack3.addEventListener("click", () => {
-    bannerBook2.style.visibility = "visible";
-    bannerBook2.style.opacity = "1";
+  bannerBook2.style.visibility = "visible";
+  bannerBook2.style.opacity = "1";
 
-    bannerBook3.style.visibility = "hidden";
-    bannerBook3.style.opacity = "0";
+  bannerBook3.style.visibility = "hidden";
+  bannerBook3.style.opacity = "0";
 
-    bannerBook1.style.visibility = "hidden";
-    bannerBook1.style.opacity = "0";
-})
+  bannerBook1.style.visibility = "hidden";
+  bannerBook1.style.opacity = "0";
+});
 
 btnNext3.addEventListener("click", () => {
-    bannerBook1.style.visibility = "visible";
-    bannerBook1.style.opacity = "1";
+  bannerBook1.style.visibility = "visible";
+  bannerBook1.style.opacity = "1";
 
-    bannerBook3.style.visibility = "hidden";
-    bannerBook3.style.opacity = "0";
+  bannerBook3.style.visibility = "hidden";
+  bannerBook3.style.opacity = "0";
 
-    bannerBook2.style.visibility = "hidden";
-    bannerBook2.style.opacity = "0";
-})
+  bannerBook2.style.visibility = "hidden";
+  bannerBook2.style.opacity = "0";
+});
 
 // Background tự chuyển
-const bookBg = document.querySelectorAll('#Good-text .bg');
+const bookBg = document.querySelectorAll("#Good-text .bg");
 let index = 0;
 
 setInterval(() => {
-    bookBg[index].classList.remove('active');
+  bookBg[index].classList.remove("active");
 
-    index++;
-    if(index >= bookBg.length) index = 0;
+  index++;
+  if (index >= bookBg.length) index = 0;
 
-    bookBg[index].classList.add('active');
-}, 12000);
-
+  bookBg[index].classList.add("active");
+}, 7000);
 
 // Cuộn đến phần sách văn học
 const Literatures = document.getElementById("Literatures");
 const literaryGenre = document.getElementById("literary-genre");
 
 Literatures.addEventListener("click", () => {
-    literaryGenre.scrollIntoView({
-        behavior: "smooth",
-        block: "start"
-    });
+  literaryGenre.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+  });
 });
 
 // Cuộn đến phần sách văn học
@@ -113,20 +111,19 @@ const Mathematics = document.getElementById("Mathematics");
 const mathematicsGenre = document.getElementById("mathematics-genre");
 
 Mathematics.addEventListener("click", () => {
-    mathematicsGenre.scrollIntoView({
-        behavior: "smooth",
-        block: "start"
-    });
+  mathematicsGenre.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+  });
 });
-
 
 // Cuộn đến phần sách tiếng Anh
 const English = document.getElementById("English");
 const englishGenre = document.getElementById("English-genre");
 
 English.addEventListener("click", () => {
-    englishGenre.scrollIntoView({
-        behavior: "smooth",
-        block: "start"
-    });
+  englishGenre.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+  });
 });
